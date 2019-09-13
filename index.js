f8388c530659a6aced3a0985a68b75c6fff19a9e
Anthony Hill <antdavhill@gmail.com>
2019-09-12 21:15:35 -0500

    added js and comments

---
+++ index.js
@@ -0,0 +1,26 @@
+function renderPage() {
+
+}
+
+function handleStartQuiz() {
+
+}
+
+function handleSubmitQuestion() {
+
+}
+
+function handleShowCorrect() {
+
+}
+
+function handleNextQuestion() {
+
+}
+
+function handleRetakeQuiz() {
+
+}
+$('.js-next').click(function(event) {
+    console.log('hey');}
+)
